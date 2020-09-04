f4e207698d67feeca0b8c57ce868b833664a0fb0
Valentin Birault <biraultvalentin@gmail.com>
2020-09-04 17:20:04 +0200

    devise + routes

--- app/controllers/api/v1/weathers_controller.rb
+++ app/controllers/api/v1/weathers_controller.rb
@@ -3,6 +3,8 @@ class Api::V1::WeathersController < Api::V1::BaseController
     location = 'Paris'
 
     @weathers = HTTParty.get("https://api.openweathermap.org/data/2.5/weather?q=#{location}&appid=#{ENV['WEATHER_API_KEY']}")
+    
+    authorize @weathers 
 
     render json: @weathers, status: :ok
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :weathers, only: [:index]
     end
+  end
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      devise_for :users
+    end 
   end
 
 #   namespace :api, defaults: { format: :json } do
